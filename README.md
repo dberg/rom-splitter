8542d9de69600445995be7f58eaea87e94c5b89a
Daniel Berg <danibberg@gmail.com>
2026-02-02 21:06:44 -0800

    Initial work parsing files.

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # Roms file splitter
 
+## Dev
+
 ```bash
 cargo build
+```
+
+Running
+
+```bash
+cargo run -- -p PATH_TO_ROMS_DIR -e nes -m 100
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,71 @@
 use std::fs;
-use std::path::Path;
+use std::path::{PathBuf};
 use ap::parser::{Parser, Policy};
 
 /// We get the `path`, `extension`, and `size` of each directory we'll produce.
+/// TODO: Handle errors
 fn main() -> std::io::Result<()> {
     let env_args = std::env::args().skip(1).collect();
-    // TODO: handle errors
-    let options = Parser::new()
-        .arg("path", 'p', Policy::Default(String::from(".")))
-        .arg("extension", 'e', Policy::Required)
-        .arg("max-roms-per-directory", 'm', Policy::Default(String::from("100")))
-        .run(&env_args).unwrap();
+    let options = Options::parse(&env_args);
 
-    // TODO: handle errors
-    let path = options.get("path").unwrap();
-    let extension = options.get("extension").unwrap();
-    let roms_per_directory = options.get("roms-per-directory").unwrap();
-    println!("path: {}, extension: {}, roms_per_directory: {}", path, extension, roms_per_directory);
-
-    // get path from command line arguments
-    let args: Vec<String> = std::env::args().collect();
-
-    if args.len() < 2 {
-        eprintln!("Usage: {} <directory>", args[0]);
-        std::process::exit(1);
-    }
-
-    let path = Path::new(&args[1]);
-
-    if !path.is_dir() {
-        eprintln!("Error: '{}' is not a directory", path.display());
-        std::process::exit(1)
-    }
-
-    println!("Files in {}:", path.display());
-
-    for entry in fs::read_dir(path)?.flatten() {
-        let path = entry.path();
-
-        // Skip directories
-        if path.is_file() {
-            if let Some(name) = path.file_name() {
-                println!("\t{}", name.to_string_lossy());
-            }
-        }
+    let files = read_rom_files_list(&options);
+    for file in files {
+        println!("{}", file.display())
     }
 
     Ok(())
+}
+
+fn read_rom_files_list(options: &Options) -> Vec<PathBuf> {
+    let mut files = Vec::new();
+
+    for entry in fs::read_dir(&options.path).unwrap() {
+        let entry = entry.unwrap();
+        let path = entry.path();
+        let extension = path.extension().unwrap().to_str().unwrap();
+        let extension_match = extension == options.extension;
+
+        if path.is_file() && extension_match {
+            files.push(path);
+        }
+    }
+
+    files
+}
+
+struct Options {
+    pub path: PathBuf,
+    pub extension: String,
+    pub max_roms_per_directory: usize,
+}
+
+impl Options {
+    fn parse(args: &Vec<String>) -> Options {
+        let options = Parser::new()
+            .arg("path", 'p', Policy::Default(String::from(".")))
+            .arg("extension", 'e', Policy::Required)
+            .arg("max-roms-per-directory", 'm', Policy::Default(String::from("100")))
+            .run(args).unwrap();
+
+        let path = options.get("path").unwrap();
+        let path = PathBuf::from(path);
+        let path = path.canonicalize().unwrap();
+
+        if !path.is_dir() {
+            eprintln!("Error: '{}' is not a directory", path.display());
+            std::process::exit(1)
+        }
+
+        let extension = options.get("extension").unwrap();
+        let extension = extension.clone();
+
+        let max_roms_per_directory = options.get("max-roms-per-directory").unwrap();
+        let max_roms_per_directory = max_roms_per_directory.parse::<usize>().unwrap();
+
+        Options {
+            path,
+            extension,
+            max_roms_per_directory
+        }
+    }
 }
